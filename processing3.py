a99f4ae760f3346290726f44a269ecc9e4191357
Leonardo Vaia <leovaia32000@gmail.com>
2024-03-22 00:13:56 +0100

    a

--- processing3.py
+++ processing3.py
@@ -15,13 +15,28 @@ def generate_combinations_parallel(args):
         new_combination = current_combination + chunk
         generate_combinations_parallel((list_of_lists, index + 1, new_combination, output, stop_event))
 
+def generate_combinations1(args):
+    list_of_lists, index, current_combination, output, stop_event = args
+    processes = []
+    
+    # Start a separate process for each chunk in the first list
+    for chunk in list_of_lists[1]:
+        process = multiprocessing.Process(target=generate_combinations_parallel, args=(list_of_lists, 2, chunk,output, stop_event))
+        process.start()
+        processes.append(process)
+
+    # Join all processes
+    for process in processes:
+        process.join()
+
 def generate_combinations(list_of_lists):
     manager = Manager()
     output = manager.Queue()
     stop_event = manager.Event()  # Event to signal stopping
-    executor = ProcessPoolExecutor()
+    executor = ProcessPoolExecutor(max_workers=multiprocessing.cpu_count())
 
     futures = []
+    
     for chunk in list_of_lists[0]:
         future = executor.submit(generate_combinations_parallel, (list_of_lists, 1, chunk, output, stop_event))
         futures.append(future)
